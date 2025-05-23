28850fe434c8a2e73153944377f9e4ebe8be25cb
Om Mishra <om.works01@gmail.com>
2025-05-24 02:14:33 +0530

    feat(auth): add auth layout and pages

---
+++ app/auth/new-verification/page.tsx
@@ -0,0 +1,10 @@
+import { NewVerificationForm } from "@/components/auth/new-verification";
+
+const NewVerificationPage = () => {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <NewVerificationForm />
+    </div>
+  );
+};
+export default NewVerificationPage;
--- lib/mail.ts
+++ lib/mail.ts
@@ -5,11 +5,11 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
   await resend.emails.send({
-    from: "onboarding@superdocs.xyz",
+    from: "onboarding@ommishra.tech",
     to: email,
     subject: "Please Verify Your Email",
     html: `
-      <h1>Verify Your Email</h1>
+      <h1>Verify Your Email For Pizza</h1>
       <p>Click the link below to verify your email</p>
       <a href="${confirmLink}">Verify Email</a>
     `,
